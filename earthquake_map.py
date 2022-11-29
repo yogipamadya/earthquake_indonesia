fd9c790a13d5e9b554ccfe569846eef5c2d4b985
yogipamadya <31554546+yogipamadya@users.noreply.github.com>
2022-11-29 11:02:56 +0700

    Update earthquake_map.py

--- earthquake_map.py
+++ earthquake_map.py
@@ -4,7 +4,7 @@ import leafmap.foliumap as leafmap
 st.set_page_config(layout="wide")
 
 
-st.title("Heatmap of Earthquake in Indonesia 2000-2020")
+st.title("Earthquake Heatmap of Indonesia 2000-2020")
 
 filepath = "https://raw.githubusercontent.com/yogipamadya/earthquake_indonesia/main/eq2000-2020.csv"
 m = leafmap.Map(center=[0,113],zoom=4)
